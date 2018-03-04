8e403eb79840b4c0ecec54d74028f0d2cda360df
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-03-04 16:35:52 -0600

    added error checking to shared memory and message queues.  create both mail boxes into oss.

--- oss.c
+++ oss.c
@@ -9,90 +9,158 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <time.h>
-
+#include <sys/msg.h>
 
 #define SHM_SIZE 100
 
 void print_usage();
 
+struct mesg_buffer {
+	long mesg_type;
+	char mesg_text[100];
+	int willTerminate; // 0 or 1
+	long timeTerminatedOn;
+	long clockIncrementedBy;
+	
+} message;
 
 
-int main (int argc, char *argv[]) {
+//Globals
+
+//message queue key
+key_t messageQueueParentKey;
+key_t messageQueueChildKey;
 
 //Shared Memory Keys
-key_t keySecondsClock = 59566;
-key_t keyNanoSecondsClock = 59567;
+key_t keySecondsClock;
+key_t keyNanoSecondsClock;
 
-//Create Shared Memory
-int shmidSecondsClock = shmget(keySecondsClock, SHM_SIZE, IPC_CREAT | 0777);
-int shmidNanoSecondsClock = shmget(keyNanoSecondsClock, SHM_SIZE, IPC_CREAT | 0777);
-
-//Attach to shared memory
-int * secondsClock = (int *)shmat(shmidSecondsClock, NULL, 0);
-int * nanoSecondsClock = (int *)shmat(shmidNanoSecondsClock, NULL, 0);
-
-
-*secondsClock = 5;
-*nanoSecondsClock = 10000;
-
-int maxProcess = 5;
-int processCount = 0;
-
+//file creation for log
+char fileName[50];
 FILE * fp;
 
-int terminateTime;
+pid_t childpid;
 
-pid_t childpid = 0;
-//pid_t endID;
+int main (int argc, char *argv[]) {
+	printf("at top of parent\n");
 
-//char fileName[50];
-//fp = fopen(fileName, "w");
+	//initalize variables
+	
+	messageQueueParentKey = 59568;
+	mesageQueueChildKey = 59569;
+	keySecondsClock = 59566;
+	keyNanoSecondsCLock = 59567;	
 
-char option;
+	int maxProcess = 5; //default max processes if not specified 
+	int processCount = 0; //current number of processes
 
+	int secondsClock;
+	int nanoSecondsCLock;
 
-while ((option = getopt(argc, argv, "s:hl:t:")) != -1){
-	switch (option){
-		case 's' : maxProcess = atoi(optarg);
-		break;
+	
+	//mssget creates a message queue and returns identifier
+	//create mail boxes for parent and child processes
 
-		case 'h': print_usage();
-		break;
-
-		case 'l': 
-//			strcpy(fileName, optarg);
-		break;
-
-		case 't':
-			terminateTime = atoi(optarg);
-		break;
-
-		default : print_usage(); 
-			exit(EXIT_FAILURE);
-	}
-}
-
-printf("in Parent - clock is %d", *secondsClock);
+//	if (int msgParentID = msgget(messageQueueKey, IPC_CREAT | 0777);
 
 
-do {
-
-	if ((childpid = fork()) == -1){
-		perror(("%s: Error:", argv[0]));
-	}
-
-	processCount++;
-//	printf("processCount is %d\n", processCount);
-
-	if ((childpid == 0)){
-		execv("./user", NULL);
+														printf("after message queue creation\n");
+//getopt
+	char option;
+	while ((option = getopt(argc, argv, "s:hl:t:")) != -1){
+		switch (option){
+			case 's' : maxProcess = atoi(optarg);
+				break;
+			case 'h': print_usage();
+				break;
+			case 'l': 
+//				strcpy(fileName, optarg);
+	//			fp = fopen(fileName, "w");
+				break;
+			case 't':
+				terminateTime = atoi(optarg);
+				break;
+			default : print_usage(); 
+				exit(EXIT_FAILURE);
+		}
 	}
 
-	if (processCount == maxProcess){
-		exit(0);
-	}	
 
-} while (processCount !=  maxProcess);
+//Create Shared Memory
+	if ((int shmidSecondsClock = shmget(keySecondsClock, SHM_SIZE, IPC_CREAT | 0777)) == -1){
+		perror("oss: could not create shared memory");
+		return 1;
+	}
+	if ((int shmidNanoSecondsClock = shmget(keyNanoSecondsClock, SHM_SIZE, IPC_CREAT | 0777)) == -1){
+		perror("oss: could not create shared memory");
+		return 1;
+	}
+
+//Attach to shared memory
+	int * secondsClock = (int *)shmat(shmidSecondsClock, NULL, 0);
+	int * nanoSecondsClock = (int *)shmat(shmidNanoSecondsClock, NULL, 0);
+
+//start the clock at 0
+	*secondsClock = 0;
+	*nanoSecondsClock = 0;
+
+//create mail box for parent
+	if (( int messageBoxParentID = msgget(messageQueueParentKey, IPC_CREAT | 0777)) == -1){
+		perror("oss: Could not create parent mail box");
+		return 1;
+	}
+
+	if (( int messageBoxChildID = msgget(messageQueueChildKey, IPC_CREAT | 0777)) == -1){
+		perror("oss: Could not create child mail box");
+		return 1;
+	}
+
+	message.mesg_type = 1; //set message type
+
+//send message to child box
+	if(msgsnd(messageBoxChildID, &message, sizeof(message), 0) == -1){
+		perror("oss: Failed to send message to child");
+	}
+
+//Critical section
+
+printf("before do loop in parent\n");
+
+//do {
+
+	if (processCount != maxProcess){
+
+		if ((childpid = fork()) == -1){
+			perror(("%s: Error:", argv[0]));
+		}
+
+		processCount++;
+//		printf("processCount is %d\n", processCount);
+
+		if ((childpid == 0)){
+			printf("Forking child\n");
+			execv("./user", NULL);
+		}
+
+	}
+			
+			sleep(10);
+			msgrcv(msgid, &message, sizeof(message), 1, 0);
+			printf("In Parent - received message Clock incremented by  %ld\n", message.timeTerminatedOn);
+//test receive message
+//printf("In Parent - received message %s\n", message.mesg_text);
+
+//msgrc to receive message
+//msgrcv(msgid, &message, sizeof(message), 1, 0);
+//} while (1);
+
+
+
+shmdt(secondsClock);
+shmdt(nanoSecondsClock);
+
+shmctl(shmidSecondsClock, IPC_RMID, NULL);
+shmctl(shmidNanoSecondsClock, IPC_RMID, NULL);
 
 return 0;
 
