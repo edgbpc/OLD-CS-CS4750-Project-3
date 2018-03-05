b84d46277b72300ad93286a5855055b515b93365
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-03-04 19:11:04 -0600

    Need to work on leg building

--- oss.c
+++ oss.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,8 +9,11 @@
 #include <sys/types.h>
 #include <time.h>
 #include <sys/msg.h>
+#include <signal.h>
+#include <errno.h>
 
 #define SHM_SIZE 100
+#define BILLION 1000000000L //taken from book
 
 void print_usage();
 
@@ -36,10 +38,15 @@ key_t keySecondsClock;
 key_t keyNanoSecondsClock;
 
 //file creation for log
-char fileName[50];
+char fileName[50] = "data.log";
 FILE * fp;
 
 pid_t childpid;
+
+
+//taken from book
+static int setperiodic(double sec);
+
 
 int main (int argc, char *argv[]) {
 	printf("at top of parent\n");
@@ -47,15 +54,24 @@ int main (int argc, char *argv[]) {
 	//initalize variables
 	
 	messageQueueParentKey = 59568;
-	mesageQueueChildKey = 59569;
+	messageQueueChildKey = 59569;
 	keySecondsClock = 59566;
-	keyNanoSecondsCLock = 59567;	
+	keyNanoSecondsClock = 59567;	
 
 	int maxProcess = 5; //default max processes if not specified 
 	int processCount = 0; //current number of processes
+	int processLimit = 100; //max number of processes allowed by assignment parameters
+	int totalProcessesCreated; //keeps track of all processes created	
 
-	int secondsClock;
-	int nanoSecondsCLock;
+	int shmidSecondsClock;
+	int shmidNanoSecondsClock;
+
+	int messageBoxParentID;
+	int messageBoxChildID;
+
+//	int secondsClock;
+//	int nanoSecondsCLock;
+	double terminateTime = 20;
 
 	
 	//mssget creates a message queue and returns identifier
@@ -74,24 +90,28 @@ int main (int argc, char *argv[]) {
 			case 'h': print_usage();
 				break;
 			case 'l': 
-//				strcpy(fileName, optarg);
-	//			fp = fopen(fileName, "w");
+				strcpy(fileName, optarg);
 				break;
 			case 't':
-				terminateTime = atoi(optarg);
+				terminateTime = atof(optarg);
 				break;
 			default : print_usage(); 
 				exit(EXIT_FAILURE);
 		}
 	}
 
+//set timer 
+	if (setperiodic(terminateTime) == -1){
+		perror("oss: failed to set run timer");
+		return 1;
+		}
 
 //Create Shared Memory
-	if ((int shmidSecondsClock = shmget(keySecondsClock, SHM_SIZE, IPC_CREAT | 0777)) == -1){
+	if ((shmidSecondsClock = shmget(keySecondsClock, SHM_SIZE, IPC_CREAT | 0777)) == -1){
 		perror("oss: could not create shared memory");
 		return 1;
 	}
-	if ((int shmidNanoSecondsClock = shmget(keyNanoSecondsClock, SHM_SIZE, IPC_CREAT | 0777)) == -1){
+	if ((shmidNanoSecondsClock = shmget(keyNanoSecondsClock, SHM_SIZE, IPC_CREAT | 0777)) == -1){
 		perror("oss: could not create shared memory");
 		return 1;
 	}
@@ -105,12 +125,12 @@ int main (int argc, char *argv[]) {
 	*nanoSecondsClock = 0;
 
 //create mail box for parent
-	if (( int messageBoxParentID = msgget(messageQueueParentKey, IPC_CREAT | 0777)) == -1){
+	if ((messageBoxParentID = msgget(messageQueueParentKey, IPC_CREAT | 0777)) == -1){
 		perror("oss: Could not create parent mail box");
 		return 1;
 	}
 
-	if (( int messageBoxChildID = msgget(messageQueueChildKey, IPC_CREAT | 0777)) == -1){
+	if ((messageBoxChildID = msgget(messageQueueChildKey, IPC_CREAT | 0777)) == -1){
 		perror("oss: Could not create child mail box");
 		return 1;
 	}
@@ -122,37 +142,56 @@ int main (int argc, char *argv[]) {
 		perror("oss: Failed to send message to child");
 	}
 
-//Critical section
 
-printf("before do loop in parent\n");
+	printf("before do loop in parent\n");
 
-//do {
 
-	if (processCount != maxProcess){
+//open file for log writing
+	fp = fopen(fileName, "a");
 
-		if ((childpid = fork()) == -1){
-			perror(("%s: Error:", argv[0]));
-		}
+//MAIN PROCESS
 
-		processCount++;
-//		printf("processCount is %d\n", processCount);
 
-		if ((childpid == 0)){
-			printf("Forking child\n");
-			execv("./user", NULL);
-		}
+	do {
 
-	}
-			
-			sleep(10);
-			msgrcv(msgid, &message, sizeof(message), 1, 0);
+		while (processCount < maxProcess){
+	
+			if ((childpid = fork()) == -1){
+				perror("oss: failed to fork child");
+			}
+
+			if ((childpid == 0)){
+				fprintf(fp, "Creating new child pid %d at my time %d.%d \n", getpid(), *secondsClock, *nanoSecondsClock);
+//				printf("Forking child\n");
+				execv("./user", NULL);
+				return 1;
+			}
+
+			//increment process count
+			processCount++;
+			totalProcessesCreated++;
+			if (totalProcessesCreated == processLimit){ //break the loop if number of processes exceed the limit
+				break;
+
+			}
+
+			//CRITICAL SECTION
+			//receive a message		
+	
+			msgrcv(messageBoxParentID, &message, sizeof(message), 1, 0);
 			printf("In Parent - received message Clock incremented by  %ld\n", message.timeTerminatedOn);
-//test receive message
-//printf("In Parent - received message %s\n", message.mesg_text);
 
-//msgrc to receive message
-//msgrcv(msgid, &message, sizeof(message), 1, 0);
-//} while (1);
+			if (*secondsClock >=2){ //break the loop of seconds exceed 2
+				break;
+			}
+
+			processCount--;
+		}
+
+	} while ( *secondsClock < 2);  //runs for 2 seconds.  secondsCLock incremented in user
+
+
+
 
 
 
@@ -164,6 +203,24 @@ shmctl(shmidNanoSecondsClock, IPC_RMID, NULL);
 
 return 0;
 
+}
+
+
+
+//TAKEN FROM BOOK
+static int setperiodic(double sec) {
+   timer_t timerid;
+   struct itimerspec value;
+   if (timer_create(CLOCK_REALTIME, NULL, &timerid) == -1)
+      return -1;
+   value.it_interval.tv_sec = (long)sec;
+   value.it_interval.tv_nsec = (sec - value.it_interval.tv_sec)*BILLION;
+   if (value.it_interval.tv_nsec >= BILLION) {
+      value.it_interval.tv_sec++;
+      value.it_interval.tv_nsec -= BILLION;
+   }
+   value.it_value = value.it_interval;
+   return timer_settime(timerid, 0, &value, NULL);
 }
 
 
