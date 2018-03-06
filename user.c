3fb52f4c0217df558b2df3a053564b79485920d1
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-03-05 20:31:53 -0600

    Finished critical section.  built messages

--- user.c
+++ user.c
@@ -16,12 +16,12 @@
 
 
 struct mesg_buffer {
-	long mesg_type;
-	char mesg_text[100];
-	int willTerminate; // 0 or 1
-	long timeTerminatedOn;
-	long clockIncrementedBy;
-	
+	long mesg_type; //always 1
+	int mesg_terminatedOnSeconds;
+	int mesg_terminatedOnNanoSeconds;
+	int mesg_childSecondsTime;
+	int mesg_childNanoSecondsTime;
+	int mesg_childPid; //to tell parent which child terminated
 } message;
 
 int main (int argc, char *argv[]) {
@@ -30,22 +30,21 @@ int main (int argc, char *argv[]) {
 	key_t messageQueueParentKey = 59568;
 	key_t messageQueueChildKey = 59569;
 
-	int messageBoxParentID;
-	int messageBoxChildID;
-
 	//Shared Memory Keys
 	key_t keySecondsClock = 59566;
 	key_t keyNanoSecondsClock = 59567;
-
+	
+	int messageBoxParentID;
+	int messageBoxChildID;
 	int shmidSecondsClock;
 	int shmidNanoSecondsClock;
 
-	int runTime;
-	int currentTime = 0; //time current process has been running
+	int totalTimeRunning = 0; //time current process has been running
 	int workTime = 15000; //amount of workTime for 1 run
+	float allotatedChildRunTime; //how long the child process is allowed to run.  determined randomly in child
 
-	int startTimeSeconds;
-	int startTimeNanoSeconds;
+	int childProcessTimeSeconds; //seconds clock for child process
+	int childProcessTimeNanoSeconds; //nanoseconds clock for child process
 
 //TESTING ONLY
 //char sampleMessage[20] = "A test message";
@@ -66,8 +65,9 @@ int main (int argc, char *argv[]) {
 
 
 	//get current times from shared memory clock
-	startTimeSeconds = *secondsClock;
-	startTimeNanoSeconds = *nanoSecondsClock;
+	//load as start times for child process for seconds and nanoseconds
+	childProcessTimeSeconds = *secondsClock;
+	childProcessTimeNanoSeconds = *nanoSecondsClock;
 
 	//message queue
 	if ((messageBoxParentID = msgget(messageQueueParentKey, 0777)) == -1){
@@ -85,36 +85,91 @@ int main (int argc, char *argv[]) {
 	srand(time(NULL));
 
 	//set child run time
-	runTime = (rand() % 1000000) + 1; //random run time between 1 and 1M
+	allotatedChildRunTime = (rand() % 1000000) + 1; //random run time between .001 and .01 seconds
 
 	//CRITICAL SECTION
 
 	do{
-		msgrcv(messageBoxChildID, &message, sizeof(message), 1, 0);
+		msgrcv(messageBoxChildID, &message, sizeof(message), 1, 0); //retrieve message from max box.  child is blocked unless there is a message to take from box
 
 		//maybe not needed
 		message.mesg_type = 1;
 
 		//check if current run exceeds total allotated time
-		if ((currentTime + workTime) > runTime){
-			workTime = runTime - currentTime; //reduce work to the amount of time remaining
+		if ((totalTimeRunning + workTime) > allotatedChildRunTime){
+			workTime = allotatedChildRunTime - totalTimeRunning; //reduce work to the amount of time remaining
 			}
 
-		//increment clock
-		*nanoSecondsClock += workTime; //shared memory time
-		currentTime += workTime; //time in tihe process
+		//increment child clocks
+		childProcessTimeNanoSeconds += workTime; //shared memory time
+		totalTimeRunning += childProcessTimeNanoSeconds; //accumlating total amount of nanoseconds child process as ran
 
-		//load data into message to send to parent
+		//increment the system clocks in shared memory
+		*nanoSecondsClock += childProcessTimeNanoSeconds;
+		*secondsClock += childProcessTimeSeconds;
+
+		//convert nanoseconds to seconds
+		if (childProcessTimeNanoSeconds > 1000000000){ //if nanoSeconds > 1000000000
+			childProcessTimeSeconds += childProcessTimeNanoSeconds/1000000000; //add number of fully divisible by nanoseconds to seconds clock.  will truncate
+			childProcessTimeNanoSeconds = childProcessTimeNanoSeconds%1000000000; //assign remainder of nanoseconds to nanosecond clock
+		}
+
+		
+
+		//if  time exceeds alloated time for entire process, terminate and send message to parent
+		if (*secondsClock >= 2){
+			message.mesg_type = 1;
+			message.mesg_terminatedOnSeconds = childProcessTimeSeconds;
+			message.mesg_terminatedOnNanoSeconds = childProcessTimeNanoSeconds; 
+			message.mesg_childSecondsTime = childProcessTimeSeconds;
+			message.mesg_childNanoSecondsTime = childProcessTimeNanoSeconds;
+			message.mesg_childPid = getpid();
 	
+			if(msgsnd(messageBoxParentID, &message, sizeof(message), 0) == -1){
+				perror("user: could not send message to parent box");
+			}
+		}
+
+		//if duration has not passed, cede critical section to another child process
+		if(totalTimeRunning < allotatedChildRunTime){
+			message.mesg_type = 1;
+			message.mesg_terminatedOnSeconds = childProcessTimeSeconds;
+			message.mesg_terminatedOnNanoSeconds = childProcessTimeNanoSeconds; 
+			message.mesg_childSecondsTime = childProcessTimeSeconds;
+			message.mesg_childNanoSecondsTime = childProcessTimeNanoSeconds;
+			message.mesg_childPid = getpid();
+	
+			if(msgsnd(messageBoxChildID, &message, sizeof(message), 0) == -1){
+				perror("user: could not send message to child box");
+
+			}
+		}
+
+		
+		//prepare message 
+		message.mesg_type = 1;
+		message.mesg_terminatedOnSeconds = childProcessTimeSeconds;
+		message.mesg_terminatedOnNanoSeconds = childProcessTimeNanoSeconds; 
+		message.mesg_childSecondsTime = childProcessTimeSeconds;
+		message.mesg_childNanoSecondsTime = childProcessTimeNanoSeconds;
+		message.mesg_childPid = getpid();
+		
+
+		
 		//send message to parent
 		if (msgsnd(messageBoxParentID, &message, sizeof(message), 0) == -1){
 			perror("user: failed to send to parent box");
 		}	
 
-	} while (currentTime < runTime);
+
+
+	} while (totalTimeRunning < allotatedChildRunTime);
+
+
 
 return 0;
 
 }
+
 
 
